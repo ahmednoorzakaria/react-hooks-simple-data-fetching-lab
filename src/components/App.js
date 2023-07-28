2936e451905a7d12768e47556f38247e493de0b4
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-07-28 16:53:38 +0300

    finished crud

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => setDogImage(data.message))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  if (!dogImage) {
+    return <p>Loading</p>;
+  }
+
+  return (
+    <div>
+      <img src={dogImage} alt="A Random Dog"></img>
+    </div>
+  );
+}
+
+export default App;
